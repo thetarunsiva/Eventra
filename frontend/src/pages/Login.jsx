087c308bc94da9ff43f9218ef0313acf106c53a4
thetarunsivae <thetarunsiva@gmail.com>
2026-06-13 14:46:04 +0530

    Change of Font styles From Inter to Outfit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -38,9 +38,13 @@ function Login() {
       }
       return (
             <div style={{ display:"flex", justifyContent:"center", alignItems:"center", height:"100vh", flexDirection:"column", gap: "12px", padding: "0 16px" }}>
-                  <h1 style={{ fontSize: "3rem", marginBottom: "4px" }}>Eventra</h1>
+                  <h1 style={{ fontSize: "2.5rem", marginBottom: "8px", fontWeight: "700", fontFamily: "Silkscreen, sans-serif", textShadow: "3px 3px 0px rgba(0,0,0,0.15)" }}>
+                        Eventra
+                  </h1>
                   <h3 style={{ color: "#4B3A32", fontWeight: "normal", fontStyle: "italic", marginBottom: "8px", textAlign: "center", maxWidth: "500px", lineHeight: "1.6" }}>Turn your college email clutter into your next opportunity.</h3>
-                  <button onClick= {handleLogin} style={{ padding: "10px 24px", border:"1px solid #ccc", borderRadius:"6px", cursor: "pointer", fontSize: "14px"}}> Continue with Google </button>
+                  <button onClick= {handleLogin} style={{ padding: "10px 24px", border:"1px solid #ccc", borderRadius:"6px", cursor: "pointer", fontSize: "14px", transform: translateY(-2) }}> 
+                        Continue with Google 
+                  </button>
                   <p style={{ color: "#999", margin: "4px 0", fontSize: "12px" }}>or Try a demo</p>
                   <div style={{ display: "flex", gap: "12px" }}>
                         <button onClick={() => handleDemoLogin("User")} style={{ padding: "10px 20px", border:"1px solid #ccc", borderRadius:"6px", cursor: "pointer", fontSize: "14px"}}> Demo User </button>
