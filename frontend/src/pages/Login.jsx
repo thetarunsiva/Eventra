da8d32ac8994dd9a1e40ea31c505cdac5c548d16
thetarunsivae <thetarunsiva@gmail.com>
2026-06-13 14:53:35 +0530

    UI Changes Pt 2

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -38,10 +38,10 @@ function Login() {
       }
       return (
             <div style={{ display:"flex", justifyContent:"center", alignItems:"center", height:"100vh", flexDirection:"column", gap: "12px", padding: "0 16px" }}>
-                  <h1 style={{ fontSize: "2.5rem", marginBottom: "8px", fontWeight: "700", fontFamily: "Silkscreen, sans-serif", textShadow: "3px 3px 0px rgba(0,0,0,0.15)" }}>
+                  <h1 style={{ fontSize: "4rem", marginBottom: "8px", fontWeight: "700", fontFamily: "Outfit, sans-serif", textShadow: "3px 3px 0px rgba(0,0,0,0.15)" }}>
                         Eventra
                   </h1>
-                  <h3 style={{ color: "#4B3A32", fontWeight: "normal", fontStyle: "italic", marginBottom: "8px", textAlign: "center", maxWidth: "500px", lineHeight: "1.6" }}>Turn college email clutter into your next opportunity.</h3>
+                  <h3 style={{ color: "#4B3A32", fontWeight: "normal", marginBottom: "8px", textAlign: "center", maxWidth: "500px", lineHeight: "1.6" }}>"Turn college email clutter into your next opportunity"</h3>
                   <button onClick= {handleLogin} style={{ padding: "10px 24px", border:"1px solid #ccc", borderRadius:"6px", cursor: "pointer", fontSize: "14px", transform: "translateY(-2px)" }}> 
                         Continue with Google 
                   </button>
